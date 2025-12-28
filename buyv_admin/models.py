4412e9ae21c8da83a11a8779a53f076d646aa4f2
AnasJafir <dr.anas.jafir@gmail.com>
2025-12-28 20:48:14 +0100

    Copy database.py and config.py, fix all relative imports

--- buyv_admin/models.py
+++ buyv_admin/models.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, String, Boolean, DateTime, Text, Foreign
 from sqlalchemy.orm import relationship, Mapped, mapped_column
 from datetime import datetime
 import uuid
-from .database import Base
+from database import Base
 
 class User(Base):
     __tablename__ = "users"
